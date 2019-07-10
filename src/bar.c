1445c7567cbf2c2b5c5d049f0abdca5983077fbc
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-07-10 15:42:49 +0200

    Some very basic functionality

--- src/bar.c
+++ src/bar.c
@@ -1,7 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "structs.h"
+#include "utils.h"
+#include "version.h"
+
+#define ARG_HELP        "help"
+
+#define HELP_MESSAGE    "NukeBAR %s\n" \
+"Usage:\n  %s COMMAND - Start the bar\n" \
+"Commands:\n"\
+"\t" ARG_HELP "\t\tShow this help message.\n" \
+""
+
+enum log_levels _log_level;
+
+const char* get_version()
+{
+#ifndef VERSION_HASH
+#define VERSION_HASH "(unknown)"
+#endif
+    return VERSION_HASH;
+}
+
+void print_help(char* name)
+{
+    const char* help_msg;
+    const char* version = get_version();
+
+    help_msg = HELP_MESSAGE;
+
+    fprintf(stdout, help_msg, version, name);
+}
 
 int main(int argc, char** argv)
 {
     argc = (int)argc;
     argv = (char**)argv;
-    return 0;
+
+    _log_level = log_tracing2 | log_tracing | log_debug | log_info | log_debug | log_error;
+
+    if (argc < 0) {
+        goto _failure;
+    }
+
+    char* name = argv[0];
+    if (argc <= 1) {
+        goto _help;
+    }
+
+    char *command = argv[1];
+    if (strcmp(command, ARG_HELP) == 0) {
+        goto _help;
+    }
+
+    struct nukebar bar = {0};
+    _trace2("Started bar %p:%u", bar, sizeof(&bar));
+
+_success:
+    return EXIT_SUCCESS;
+_failure:
+    return EXIT_FAILURE;
+_help:
+    print_help(name);
+    goto _success;
 }
---
+++ src/utils.h
@@ -0,0 +1,83 @@
+#ifndef NUKEBAR_UTILS_H
+#define NUKEBAR_UTILS_H
+
+#include <stdbool.h>
+
+#define LOG_ERROR_LABEL "ERROR"
+#define LOG_WARNING_LABEL "WARNING"
+#define LOG_DEBUG_LABEL "DEBUG"
+#define LOG_INFO_LABEL "INFO"
+#define LOG_TRACING_LABEL "TRACING"
+#define LOG_TRACING2_LABEL "TRACING"
+
+enum log_levels
+{
+    log_none     = 0U,
+    log_error    = (1U << 0U),
+    log_warning  = (1U << 1U),
+    log_info     = (1U << 2U),
+    log_debug    = (1U << 3U),
+    log_tracing  = (1U << 4U),
+    log_tracing2 = (1U << 5U),
+};
+
+static bool level_is(unsigned incoming, enum log_levels level)
+{
+    return ((incoming & level) == level);
+}
+
+static const char *get_log_level (enum log_levels l)
+{
+    if (level_is(l, log_tracing2)) { return LOG_TRACING2_LABEL; }
+    if (level_is(l, log_tracing)) { return LOG_TRACING_LABEL; }
+    if (level_is(l, log_debug)) { return LOG_DEBUG_LABEL; }
+    if (level_is(l, log_info)) { return LOG_INFO_LABEL; }
+    if (level_is(l, log_warning)) { return LOG_WARNING_LABEL; }
+    if (level_is(l, log_error)) { return LOG_ERROR_LABEL; }
+    return LOG_TRACING_LABEL;
+}
+
+#define _error(...) _logd(log_error, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define _warn(...) _logd(log_warning, __FILE__, __func__, __LINE__,  __VA_ARGS__)
+#define _info(...) _logd(log_info, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define _debug(...) _logd(log_debug, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#ifdef DEBUG
+#define _trace(...) _logd(log_tracing, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define _trace2(...) _logd(log_tracing2, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#else
+#define _trace(...)
+#define _trace2(...)
+#endif
+
+int _logd(enum log_levels level, const char *file, const char *function, const int line, const char *format, ...)
+{
+    extern enum log_levels _log_level;
+    if (!level_is(_log_level, level)) { return 0; }
+
+    va_list args;
+    va_start(args, format);
+
+    const char *label = get_log_level(level);
+    size_t p_len = strlen(LOG_WARNING_LABEL) + 1;
+
+    char *suffix = calloc(1, 1024);
+    snprintf(suffix, 1024, " in %s:%d::%s()\n", file, line, function);
+
+    size_t s_len = strlen(suffix);
+    size_t f_len = strlen(format);
+    size_t full_len = p_len + f_len + s_len + 1;
+
+    char log_format[full_len];
+    memset(log_format, 0x0, full_len);
+    snprintf(log_format, p_len + 1, "%-7s ", label);
+
+    strncat(log_format, format, f_len + 1);
+    strncat(log_format, suffix, s_len + 1);
+
+    int result = vfprintf(stderr, log_format, args);
+    va_end(args);
+
+    return result;
+}
+
+#endif // NUKEBAR_UTILS_H
